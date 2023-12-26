b0949e48709743c67f7bed386b4dac916fb1076a
Smit <smit.fst.1053@gmail.com>
2023-12-26 14:36:11 +0530

    Updated auth

--- pages/api/hello2.ts
+++ pages/api/hello2.ts
@@ -1,12 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { authService } from '../../shared/services/auth';
+
+interface Request extends NextApiRequest {
+    userId: string;
+}
 
 type ResponseData = {
     message: string;
 };
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<ResponseData>
+export default async function handler(
+    req: Request,
+    res: NextApiResponse
 ) {
-    res.status(200).json({ message: 'Hello from Next.js!' });
+    const user = await authService.authenticate(req);
+
+    res.status(200).json({ message: user });
 }
