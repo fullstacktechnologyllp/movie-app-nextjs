63d3c18e20ddab644d54fc0fc7088a8448a3f572
Smit <smit.fst.1053@gmail.com>
2023-12-27 11:50:04 +0530

    Fix css issue in api doc page

---
+++ src/app/api-doc/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './api-doc.css';
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Movie App',
+  description: '',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
