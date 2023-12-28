fe01b96ced7ec1c6089ec48365e0f908889ad482
Smit <smit.fst.1053@gmail.com>
2023-12-28 18:39:18 +0530

    added default border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,19 @@ function Home() {
 
   useEffect(() => {
     const token = localStorage.getItem('token');
+    const selectedLanguage = localStorage.getItem('selectedLanguage');
+
     if (token !== '') {
       setLoggedIn(true);
       window.location.href = '/movies'
     }
+    if (!selectedLanguage) {
+      localStorage.setItem('selectedLanguage','en');
+    }
   }, []);
 
   return (<>
- { !isLoggedIn ? <><Signin /></> : <><Movies /></> }
+    { !isLoggedIn ? <><Signin /></> : <><Movies /></> }
 
   </>)
 }
